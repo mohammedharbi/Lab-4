7dbe9b480876cc500a460da6af1cd1a468b007ed
mohammed alharbi <mohammedsfh77@gmail.com>
2024-11-07 11:38:37 +0300

    Lab 4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        DecimalFormat d1 = new DecimalFormat("#.##");
         Circle c1 = new Circle(4);
 
         System.out.println("---------Circle---------");
         System.out.println("The radius of a circle:"+c1.getRadius()+"\n" +
                 "The Position of circle: x= "+c1.getX()+" Y= "+c1.getY()+"\n" +
-                "The Area: "+c1.calculateArea()+"\n" +
-                "The Circumference: "+c1.calculateCircumference());
+                "The Area: "+d1.format(c1.calculateArea())+"\n" +
+                "The Circumference: "+d1.format(c1.calculateCircumference()));
         c1.setRadius(5);
         c1.setX(2);
         c1.setY(2);
@@ -17,13 +21,12 @@ public class Main {
                 "The Position of circle: x= "+c1.getX()+" Y= "+c1.getY()+"\n");
 
         System.out.println("---------Rectangle---------");
-
         Rectangle r1 = new Rectangle(30,20);
 
         System.out.println("The length of a Rectangle: "+r1.getLength()+", and width= "+r1.getWidth()+"\n" +
                 "The Position of Rectangle: x= "+r1.getX()+" Y= "+r1.getY()+"\n" +
-                "The Area: "+r1.calculateArea()+"\n" +
-                "The Circumference: "+r1.calculateCircumference());
+                "The Area: "+d1.format(r1.calculateArea())+"\n" +
+                "The Circumference: "+d1.format(r1.calculateCircumference()));
         r1.setLength(5);
         r1.setWidth(10);
         System.out.println("\nThe length of a Rectangle: "+r1.getLength()+", and width= "+r1.getWidth());
@@ -34,8 +37,8 @@ public class Main {
 
         System.out.println("The Height of a Triangle:"+t1.getHeight()+", base= "+t1.getBase()+"\n" +
                 "The Position of Triangle: x= "+t1.getX()+" Y= "+t1.getY()+"\n" +
-                "The Area: "+t1.calculateArea()+"\n" +
-                "The Circumference: "+t1.calculateCircumference());
+                "The Area: "+d1.format(t1.calculateArea())+"\n" +
+                "The Circumference: "+d1.format(t1.calculateCircumference()));
         t1.setHeight(5);
         t1.setBase(2.5);
         System.out.println("\nThe Height of a Triangle:"+t1.getHeight()+", base= "+t1.getBase());
